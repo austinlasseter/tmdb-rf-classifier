514cff11dad02f72e3047e6db2c6b2f08a803fd8
Austin Lasseter <austinlasseter@gmail.com>
2020-02-24 14:28:19 -0500

    testing the vectorizer

--- app.py
+++ app.py
@@ -22,9 +22,8 @@ sourceurl2 = 'https://www.themoviedb.org/'
 githublink = 'https://github.com/austinlasseter/movie_genres'
 
 
-
-file = open('vectorizer.pkl', 'rb')
-vec_pkl=pickle.load(file)
+file = open('analysis/vectorizer.pkl', 'rb')
+tvec_pkl=pickle.load(file)
 file.close()
 
 
@@ -38,112 +37,112 @@ app.title=tabtitle
 ########### Layout
 
 app.layout = html.Div(children=['test',
-    # dcc.Store(id='tmdb-store', storage_type='session'),
-    # dcc.Store(id='summary-store', storage_type='session'),
-    # html.Div([
-    #     html.H4(['Genre Game']),
-    #     html.Div(str(sklearn.__version__)),
-    #     html.Div('Press the button to pick a movie'),
-    #     html.Button(id='bam-button', n_clicks=0, children='BAM!'),
-    #     html.Div(id='movie-title', children=[]),
-    #     html.Div(id='movie-release', children=[]),
-    #     html.Div(id='movie-overview', children=[]),
-    #     html.Br(),
-    #     dcc.Input(
-    #         id='summary-input',
-    #         type='text',
-    #         size='100',
-    #         placeholder='Type or paste a movie summary here!',
-    #     ),
-    #     html.Button(id='biff-button', n_clicks=0, children='BIFF!'),
-    #     html.Div(id='summary-output', children='Press the button!'),
-    # ], className='twelve columns'),
-    #
-    #
-    #     # Output
-    # html.Div([
-    #     # Footer
-    #     html.Br(),
-    #     html.A('Code on Github', href=githublink, target="_blank"),
-    #     html.Br(),
-    #     html.A("Data Source: Kaggle", href=sourceurl, target="_blank"),
-    #     html.Br(),
-    #     html.A("Data Source: TMDB", href=sourceurl2, target="_blank"),
-    # ], className='twelve columns'),
+    dcc.Store(id='tmdb-store', storage_type='session'),
+    dcc.Store(id='summary-store', storage_type='session'),
+    html.Div([
+        html.H4(['Genre Game']),
+        html.Div(str(sklearn.__version__)),
+        html.Div('Press the button to pick a movie'),
+        html.Button(id='bam-button', n_clicks=0, children='BAM!'),
+        html.Div(id='movie-title', children=[]),
+        html.Div(id='movie-release', children=[]),
+        html.Div(id='movie-overview', children=[]),
+        html.Br(),
+        dcc.Input(
+            id='summary-input',
+            type='text',
+            size='100',
+            placeholder='Type or paste a movie summary here!',
+        ),
+        html.Button(id='biff-button', n_clicks=0, children='BIFF!'),
+        html.Div(id='summary-output', children='Press the button!'),
+        html.Div(id='vectorized', children=str(tvec_pkl.transform(['this is a test this is just a test']))),
+    ], className='twelve columns'),
+
+
+        # Output
+    html.Div([
+        # Footer
+        html.Br(),
+        html.A('Code on Github', href=githublink, target="_blank"),
+        html.Br(),
+        html.A("Data Source: Kaggle", href=sourceurl, target="_blank"),
+        html.Br(),
+        html.A("Data Source: TMDB", href=sourceurl2, target="_blank"),
+    ], className='twelve columns'),
 
 
 
     ]
 )
 
-########### Callbacks
-#
-# # TMDB API call
-# @app.callback(Output('tmdb-store', 'data'),
-#               [Input('bam-button', 'n_clicks')],
-#               [State('tmdb-store', 'data')])
-# def on_click(n_clicks, data):
-#     if n_clicks is None:
-#         raise PreventUpdate
-#     elif n_clicks==0:
-#         data = {'title':'please make a selection', 'release_date':' ', 'overview':' '}
-#     elif n_clicks>0:
-#         data = api_pull(random.choice(ids_list))
-#     return data
-#
-# @app.callback(Output('movie-title', 'children'),
-#               [Input('tmdb-store', 'modified_timestamp')],
-#               [State('tmdb-store', 'data')])
-# def on_data(ts, data):
-#     if ts is None:
-#         raise PreventUpdate
-#     else:
-#         return data['title']
-#
-# @app.callback(Output('movie-release', 'children'),
-#               [Input('tmdb-store', 'modified_timestamp')],
-#               [State('tmdb-store', 'data')])
-# def on_data(ts, data):
-#     if ts is None:
-#         raise PreventUpdate
-#     else:
-#         return data['release_date']
-#
-# @app.callback(Output('movie-overview', 'children'),
-#               [Input('tmdb-store', 'modified_timestamp')],
-#               [State('tmdb-store', 'data')])
-# def on_data(ts, data):
-#     if ts is None:
-#         raise PreventUpdate
-#     else:
-#         return data['overview']
-#
-# # User writes their own summary
-#
-# @app.callback(Output('summary-store', 'data'),
-#               [Input('biff-button', 'n_clicks')],
-#               [State('summary-input', 'value')]
-#               )
-# def on_click(n_clicks, value):
-#     if n_clicks is None:
-#         raise PreventUpdate
-#     elif n_clicks==0:
-#         data = 'your text will be displayed here after you press the button'
-#     elif n_clicks>0:
-#         data = str(value)
-#     return data
-#
-#
-# @app.callback(Output('summary-output', 'children'),
-#               [Input('summary-store', 'modified_timestamp')],
-#               [State('summary-store', 'data')])
-# def on_data(ts, data):
-#     if ts is None:
-#         raise PreventUpdate
-#     else:
-#         return data
+########## Callbacks
 
-# remove the pickle files, they are already zipped.
+# TMDB API call
+@app.callback(Output('tmdb-store', 'data'),
+              [Input('bam-button', 'n_clicks')],
+              [State('tmdb-store', 'data')])
+def on_click(n_clicks, data):
+    if n_clicks is None:
+        raise PreventUpdate
+    elif n_clicks==0:
+        data = {'title':'please make a selection', 'release_date':' ', 'overview':' '}
+    elif n_clicks>0:
+        data = api_pull(random.choice(ids_list))
+    return data
+
+@app.callback(Output('movie-title', 'children'),
+              [Input('tmdb-store', 'modified_timestamp')],
+              [State('tmdb-store', 'data')])
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+    else:
+        return data['title']
+
+@app.callback(Output('movie-release', 'children'),
+              [Input('tmdb-store', 'modified_timestamp')],
+              [State('tmdb-store', 'data')])
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+    else:
+        return data['release_date']
+
+@app.callback(Output('movie-overview', 'children'),
+              [Input('tmdb-store', 'modified_timestamp')],
+              [State('tmdb-store', 'data')])
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+    else:
+        return data['overview']
+
+# User writes their own summary
+
+@app.callback(Output('summary-store', 'data'),
+              [Input('biff-button', 'n_clicks')],
+              [State('summary-input', 'value')]
+              )
+def on_click(n_clicks, value):
+    if n_clicks is None:
+        raise PreventUpdate
+    elif n_clicks==0:
+        data = 'your text will be displayed here after you press the button'
+    elif n_clicks>0:
+        data = str(value)
+    return data
+
+
+@app.callback(Output('summary-output', 'children'),
+              [Input('summary-store', 'modified_timestamp')],
+              [State('summary-store', 'data')])
+def on_data(ts, data):
+    if ts is None:
+        raise PreventUpdate
+    else:
+        return data
+
 
 
 ############ Deploy
